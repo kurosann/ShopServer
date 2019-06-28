d5d67d7a4463c8286aa00f58f154be681af08d38
kuro <1343350687@qq.com>
2019-06-28 20:54:23 +0800

    支持访问本地图片

--- src/main/java/com/example/demo/controller/HTMLController.java
+++ src/main/java/com/example/demo/controller/HTMLController.java
@@ -2,13 +2,25 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-public class HTMLController {
+public class HTMLController implements WebMvcConfigurer {
 
     @RequestMapping("/upload")
     public String upload() {
         return "upload";
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //和页面有关的静态目录都放在项目的static目录下
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+
+        //上传的图片在D盘下的OTA目录下，访问路径如：http://localhost:8081/OTA/d3cf0281-bb7f-40e0-ab77-406db95ccf2c.jpg
+        //其中OTA表示访问的前缀。"file:D:/OTA/"是文件真实的存储路径
+        registry.addResourceHandler("/OTA/**").addResourceLocations("file:D:/OTA/");
+    }
+
 }
