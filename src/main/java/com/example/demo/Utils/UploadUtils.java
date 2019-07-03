49d7703e966700379de586d9791af70708edaaee
kuro <1343350687@qq.com>
2019-07-03 14:01:49 +0800

    更新上传文件路径

--- src/main/java/com/example/demo/Utils/UploadUtils.java
+++ src/main/java/com/example/demo/Utils/UploadUtils.java
@@ -3,7 +3,7 @@ package com.example.demo.Utils;
 import java.io.File;
 
 public class UploadUtils {
-    public static final String IMG_RATH_PREFIX = "static/upload/img";
+    public static final String IMG_RATH_PREFIX = "static/upload";
 
     public static File getImgDirFile() {
         String fileDirPath = "src/main/resources/" + IMG_RATH_PREFIX;
